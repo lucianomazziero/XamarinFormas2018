68ee87f1c770498aee3122573acac2a9f98c1fa9
Luciano Mazziero <lucianomazziero@hotmail.com>
2018-05-29 14:34:14 -0300

    App01 - Concluido

---
+++ Seção 05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultaCEP.Servico.Modelo;
+using App01_ConsultaCEP.Servico;
+
+
+namespace App01_ConsultaCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            //Logo.Opacity = 10;
+            BOTAO.Clicked += BuscarCEP;
+		}
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {// TODO - Lógica do Programa
+
+            
+            // TODO - Validações
+            string cep = CEP.Text.Trim();
+            
+            if ( isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep); // CEP vem lá da tela do x:Name="CEP"
+
+                    if ( end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço:{2}, {3}, {0} - {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                        RESULTADO.TextColor = Color.Black;
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO CRITICO", "O Endereço não coi encontrado para o CEP digitado: " + CEP.Text.Trim(), "OK");
+                    }
+
+                }
+                catch(Exception e) { DisplayAlert("ERRO CRITICO", e.Message, "OK"); }
+
+            }
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            /// validando de tem 8 digitos
+            if ( cep.Length != 8)
+            {
+                /// este mostra uma messagebox:  DisplayAlert("ERRO", "CEP Inválido! O CEP deve conter 8 caractéres.", "OK");
+                RESULTADO.Text = "CEP Inválido! O CEP deve conter 8 caractéres.";
+                RESULTADO.TextColor = Color.Red;
+                valido = false;
+            }
+            else
+            {
+                //// validando se é numero inteiro
+                int NovoCEP = 0;
+                if (!int.TryParse(cep, out NovoCEP))
+                {
+                    /// este mostra uma messagebox:  DisplayAlert("ERRO", "CEP Inválido! O CEP é formado por apenas numeros.", "OK"); /// retorna mensagem de erro
+                    RESULTADO.Text = "O CEP é formado por apenas numeros.";
+                    RESULTADO.TextColor = Color.Red;
+                    valido = false;
+                }
+            }
+
+
+
+            return valido;
+        }
+	}
+}
---
+++ Seção 05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Servico/Modelo/ViaCEPServico.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;   /// incluida para pegar o Webclient
+using App01_ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+
+
+namespace App01_ConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public object end2;
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEndereoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEndereoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+            
+            if (end.cep == null) { return null; }
+
+            return end;
+        }
+
+
+
+
+    }
+}
